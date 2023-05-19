d7e1672e254d68d3b81e26634f6bbedc4ffc6ff9
AlmogShor <75311629+AlmogShor@users.noreply.github.com>
2023-05-19 19:53:11 +0300

    delete comletley team.cpp content and create Point.cpp content

--- sources/Point.cpp
+++ sources/Point.cpp
@@ -3,3 +3,34 @@
 //
 
 #include "Point.hpp"
+
+#include <stdexcept>
+#include <iostream>
+
+namespace ariel {
+    Point::Point(double x, double y) : x(x), y(y) {}
+
+    double Point::distance(const Point &other) const {
+        double dx = x - other.x;
+        double dy = y - other.y;
+        return std::sqrt(dx * dx + dy * dy);
+    }
+
+    Point Point::moveTowards(const Point &source, const Point &target, double distance) {
+        if (distance < 0) {
+            throw std::invalid_argument("Distance can't be negative");
+        }
+        double totalDistance = source.distance(target);
+        if (distance > totalDistance) {
+            return target;
+        }
+        double ratio = distance / totalDistance;
+        double newX = source.x + ratio * (target.x - source.x);
+        double newY = source.y + ratio * (target.y - source.y);
+        return Point(newX, newY);
+    }
+
+    void Point::print() const {
+        std::cout << "Point(" << x << ", " << y << ")\n";
+    }
+}
